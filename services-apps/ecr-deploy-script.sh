8107c7ce3b94412e1d8400304355c87f0901127b
Soe Tint Aung <soetintaunghz10@gmail.com>
2025-04-14 19:01:19 +0630

    add:s3

--- services-apps/ecr-deploy-script.sh
+++ services-apps/ecr-deploy-script.sh
@@ -4,26 +4,27 @@ aws ecr get-login-password --region eu-west-1 | docker login --username AWS --pa
 
 docker build -t uno ./uno
 # docker buildx build --platform=linux/amd64 -t uno ./uno        !!In case you use apple m1 chip use this command instead of the first!!
-docker tag uno <YOUR AWS ACCOUNT ID>.dkr.ecr.eu-west-1.amazonaws.com/fgms-uno:v1
-docker push <YOUR AWS ACCOUNT ID>.dkr.ecr.eu-west-1.amazonaws.com/fgms-uno:v1
+docker tag uno <YOUR AWS ACCOUNT ID>.dkr.ecr.eu-west-1.amazonaws.com/sta-uno:v1
+docker push <YOUR AWS ACCOUNT ID>.dkr.ecr.eu-west-1.amazonaws.com/sta-uno:v1
 
-aws ecs update-service --cluster fgms_ecs_cluster --service fgms_uno_td_service --force-new-deployment
+aws ecs update-service --cluster sta_ecs_cluster --service sta_uno_td_service --force-new-deployment
 
 
 
 docker build -t due ./due
 # docker buildx build --platform=linux/amd64 -t due ./due        !!In case you use apple m1 chip use this command instead of the first!!
-docker tag due <YOUR AWS ACCOUNT ID>.dkr.ecr.eu-west-1.amazonaws.com/fgms-due:v1
-docker push <YOUR AWS ACCOUNT ID>.dkr.ecr.eu-west-1.amazonaws.com/fgms-due:v1
+docker tag due <YOUR AWS ACCOUNT ID>.dkr.ecr.eu-west-1.amazonaws.com/sta-due:v1
+docker push <YOUR AWS ACCOUNT ID>.dkr.ecr.eu-west-1.amazonaws.com/sta-due:v1
 
-aws ecs update-service --cluster fgms_ecs_cluster --service fgms_due_td_service --force-new-deployment
+aws ecs update-service --cluster sta_ecs_cluster --service sta_due_td_service --force-new-deployment
 
 
 
 docker build -t tre ./tre
-# docker buildx build --platform=linux/amd64 -t tre ./tre        !!In case you use apple m1 chip use this command instead of the first!!docker tag tre <YOUR AWS ACCOUNT ID>.dkr.ecr.eu-west-1.amazonaws.com/fgms-tre:v1
-docker push <YOUR AWS ACCOUNT ID>.dkr.ecr.eu-west-1.amazonaws.com/fgms-tre:v1
+# docker buildx build --platform=linux/amd64 -t tre ./tre        !!In case you use apple m1 chip use this command instead of the first!!
+docker tag tre <YOUR AWS ACCOUNT ID>.dkr.ecr.eu-west-1.amazonaws.com/sta-tre:v1
+docker push <YOUR AWS ACCOUNT ID>.dkr.ecr.eu-west-1.amazonaws.com/sta-tre:v1
 
-aws ecs update-service --cluster fgms_ecs_cluster --service fgms_tre_td_service --force-new-deployment
+aws ecs update-service --cluster sta_ecs_cluster --service sta_tre_td_service --force-new-deployment
 
 
